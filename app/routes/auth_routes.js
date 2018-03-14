996d27c907bae312bcd89d84ae18eb537b8f542d
Gaurav Sarma <gaurav4sarma@gmail.com>
2018-03-14 22:36:26 +0530

    Added query to check if username already exists during sign up

--- app/routes/auth_routes.js
+++ app/routes/auth_routes.js
@@ -3,7 +3,7 @@ var ObjectID = require('mongodb').ObjectID
 module.exports = function(app, db) {
 
 	app.get('/', (req, res) => {
-		res.send("Welcome to teqnihome heroku app");
+		res.send("Welcome to teqnihome");
 	});
 
 	app.get('/user/:id', (req, res) => {
@@ -11,39 +11,45 @@ module.exports = function(app, db) {
 		const details = {'_id': new ObjectID(id)};
 		db.collection('users').findOne(details, (err, item) => {
 			if(err) {
-				res.send({ 'error': 'An error has occured'});
+				res.send({'status': 'error', 'error': 'An error has occured'});
 			} else {
-				res.send(item);
+				res.send({'status': 'ok', 'data': item});
 			}
 		})
 	});
 
 	app.post('/register', (req, res) => {
 		
-		if(db.collection('users').find({'username': {$eq : req.body.username}})){
-			res.send({ 'error': 'Username already exists.'});
-		} else {
-			const user = { username: req.body.username, password: req.body.password };
-			db.collection('users').insert(user, (err, results) => {
-				if(err) {
-					res.send({ 'error': 'An error has occured'});
+		db.collection('users').findOne({"username": req.body.username}, (err, item) => {
+			if(err) {
+				res.send({'status':'error', 'message': 'An error has occured'});
+			} else {
+				if(item != null) {
+					res.send({'status':'error', 'message': 'Username already exists'});
 				} else {
-					res.send(results.ops[0]);
+					const user = { username: req.body.username, password: req.body.password };
+					db.collection('users').insert(user, (err, results) => {
+						if(err) {
+							res.send({'status':'error', 'message': 'An error has occured'});
+						} else {
+							res.send({'status': 'ok', 'data': results.ops[0]});
+						}
+					})	
 				}
-			})	
-		}
+			}
+		})
 	});
 
 	app.post('/login', (req, res) => {
 		const user = { 'username': req.body.username, 'password': req.body.password };
 		db.collection('users').findOne(user, (err, item) => {
 			if(err) {
-				res.send({ 'error': 'An error has occured.'});
+				res.send({'status':'error', 'message': 'An error has occured'});
 			} else {
 				if(item != null) {
-					res.send(item);
+					res.send({'status': 'ok', 'data': item});
 				} else {
-					res.send({'error': 'Username/password doesn\'t exits.'});
+					res.send({'status':'error', 'message': 'Username/password doesn\'t exits.'});
 				}
 			}
 		})
